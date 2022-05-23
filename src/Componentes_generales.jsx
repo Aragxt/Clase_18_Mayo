5eced50d1035a0f9761f5fa2d1946f340c3ece02
Your Name <hriosi@unal.edu.co>
2022-05-23 14:03:02 -0500

    Subo parte correctamente HR

--- src/Componentes_generales.jsx
+++ src/Componentes_generales.jsx
@@ -1,12 +1,23 @@
 import React from 'react';
 import 'bootstrap/dist/css/bootstrap.min.css';
-import {Alerta} from './Alerta';
 
-
+import {Formulario} from './Formulario';
+import { Barr } from './Barr';
+import { Alerta } from './Alerta';
+import { Piepag } from './Piepag';
+import {Registro} from './Registro';
 export function Aplicacion(){
     return(
         <React.Fragment>
+            <Barr/>
             <Alerta/>
+            <Alerta/>
+            <Alerta/>
+            <Alerta/>
+            <Alerta/>
+            <Alerta/>
+            <Registro/>
+            <Piepag/>
         </React.Fragment>
     )
 }
