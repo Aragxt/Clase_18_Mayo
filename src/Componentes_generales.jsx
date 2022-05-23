2d2d4cf8404c96302e78304f0f2ba5810bba6879
saira salazar <SairaSN>
2022-05-22 21:38:10 -0500

    Subo parte SS

---
+++ src/Componentes_generales.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import {Alerta} from './Alerta';
+
+
+export function Aplicacion(){
+    return(
+        <React.Fragment>
+            <Alerta/>
+        </React.Fragment>
+    )
+}
